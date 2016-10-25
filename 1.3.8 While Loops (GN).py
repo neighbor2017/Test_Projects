e01b98ec49b556c567346a7cb0d9e6929d7c4c92
neighbor2017 <2017-gneighbor@alburnettcsd.org>
2016-10-25 07:34:20 -0700

    Create Start Program

---
+++ 1.3.8 While Loops (GN).py
@@ -0,0 +1,7 @@
+
+def validate():
+    guess = '' # initialization with a bad guess
+    answer = 'hangman word'
+    while guess not in answer:
+        guess = raw_input('Name a letter in \''+answer+'\': ')
+    print('Thank you!')
